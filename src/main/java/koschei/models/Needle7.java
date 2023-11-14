1f947dfb7cbc8bedce81c69a4142a3be209b68f2
CatsAreAmazing <mrddima1@gmail.com>
2023-11-14 03:06:53 +0300

    Задание выполнено

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -1,6 +1,18 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Needle7 {
+
+    private final Deth8 death; //Только не очень понятно зачем, когда с нее ничего не идет,
+    // но допустим, что для последнего варианта связи
+
+    @Autowired
+    public Needle7(Deth8 death) {
+        this.death = death;
+    }
 
     @Override
     public String toString() {
